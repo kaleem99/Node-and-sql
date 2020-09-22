a881b424fe68177290fe1bcc5f5d35732d3ace4d
kaleem99 <kaleem1999@outlook.com>
2020-09-22 18:27:39 +0200

    initial commit

---
+++ spec/app_spec.js
@@ -0,0 +1,59 @@
+let sourceFile = require('../src/app');
+
+
+it('should create a table called visitor', async(done) => {
+    if(!sourceFile.createTable) {
+          expect(sourceFile.createTable).toBeNull();  
+    }else {
+        expect(sourceFile.createTable).not.toBeNull();
+    }
+    done()
+})
+
+it('should add some data to table', async(done) => {
+    let visitorName = sourceFile.addNewVisitor(arguments[0]);
+    let visitorAge = sourceFile.addNewVisitor(arguments[1]);
+    let dateVisited = sourceFile.addNewVisitor(arguments[2]);
+    let timeVisited = sourceFile.addNewVisitor(arguments[3]);
+    let assistedBy = sourceFile.addNewVisitor(arguments[4])
+    let comments = sourceFile.addNewVisitor(arguments[5])
+
+    expect(visitorName).not.toBeNull();
+    expect(visitorAge).not.toBeNull();
+    expect(dateVisited).not.toBeNull();
+    expect(timeVisited).not.toBeNull();
+    expect(assistedBy).not.toBeNull();
+    expect(comments).not.toBeNull();
+    done()
+});
+
+it('should list all visitors', () => {
+    expect(sourceFile.listAllVisitors).toBeDefined();
+
+});
+
+it('should delete a visitor', async (done) => {
+    let deleteVisitor = sourceFile.deleteAvisitor();
+    let numBefore;
+    if(numBefore = 2){
+        expect(deleteVisitor).toBeLessThan(numBefore)
+    }
+    done();
+});
+
+it('should update a visitor', async (done) => {
+    expect(sourceFile.updateAvisitor).toBeGreaterThan('number before');
+    done();
+});
+
+it('should return data for a visitor', async(id, done) => {
+    expect(sourceFile.viewOnevisitor).toBeDefined();
+    done();
+});
+
+it('should delete all visitors', async(done) => {
+
+    sourceFile.deleteAllVisitors;
+       expect(sourceFile.deleteAllVisitors).toBeNull();
+    done();
+});
